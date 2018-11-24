a1ef4187df2515709c2a9ddcdb4799e7edc3ad04
Daniel Lewis <daniellewis777@gmail.com>
2018-11-24 15:27:27 +0000

    added ignore for error that should never throw

--- js/getPrimeNumbers.js
+++ js/getPrimeNumbers.js
@@ -4,6 +4,7 @@ const isPrime = require('is-prime')
 const getPrimeNumbers = max => {
   const numbers = primes(max + 1)
   numbers.forEach(num => {
+    /* istanbul ignore next */
     if (!isPrime(num)) throw new Error(`${num} is not a prime number`)
   })
   return numbers
