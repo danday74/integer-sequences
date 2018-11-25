344c8bbdba36c0ffb2ec2a8cf53bdc919c7757f9
Daniel Lewis <daniellewis777@gmail.com>
2018-11-25 01:41:44 +0000

    Added prime generator

---
+++ generate-primes.js
@@ -0,0 +1,15 @@
+const main = require('./index')
+
+const arg = process.argv[2]
+
+function isInteger(str) {
+  const n = Math.floor(Number(str))
+  return n !== Infinity && String(n) === str && n >= 0
+}
+
+if (isInteger(arg)) {
+  const num = parseInt(arg)
+  console.time('generate-primes')
+  main.getPrimeNumbers(num)
+  console.timeEnd('generate-primes')
+}
