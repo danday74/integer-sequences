ec355c39e4fa115a4dd40cdf547ba26af5645624
Daniel Lewis <daniellewis777@gmail.com>
2018-12-21 01:47:10 +0000

    cubic tests to shared

--- js/cubic-numbers/get-nth-cubic-number.spec.js
+++ js/cubic-numbers/get-nth-cubic-number.spec.js
@@ -1,47 +1,7 @@
 const Big = require('big.js')
-const chai = require('chai')
-const expect = chai.expect
 const main = require('../..')
+const spec = require('../specs/get-nth-number-spec')
 
 describe('getNthCubicNumber', () => {
-
-  it('null', () => {
-    const number = main.getNthCubicNumber()
-    expect(number).to.be.undefined
-  })
-
-  it('C0', () => {
-    const number = main.getNthCubicNumber(0)
-    expect(number).to.be.undefined
-  })
-
-  it('C1', () => {
-    const number = main.getNthCubicNumber(1)
-    expect(number).to.eql(Big(1))
-  })
-
-  it('C2', () => {
-    const number = main.getNthCubicNumber(2)
-    expect(number).to.eql(Big(8))
-  })
-
-  it('C3', () => {
-    const number = main.getNthCubicNumber(Big(3))
-    expect(number).to.eql(Big(27))
-  })
-
-  it('C4', () => {
-    const number = main.getNthCubicNumber(Big(4))
-    expect(number).to.eql(Big(64))
-  })
-
-  it('C5', () => {
-    const number = main.getNthCubicNumber('5')
-    expect(number).to.eql(Big(125))
-  })
-
-  it('C73', () => {
-    const number = main.getNthCubicNumber('73')
-    expect(number).to.eql(Big(389017))
-  })
+  spec(main.getNthCubicNumber, 'C', [Big(1), Big(8), Big(27), Big(64), Big(125), Big(389017)])
 })
