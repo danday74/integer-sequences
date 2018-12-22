ea969a267ae1a50a10b727632c161efb7cca2d1d
Daniel Lewis <daniellewis777@gmail.com>
2018-12-22 01:39:35 +0000

    test params to big strings

--- js/square-numbers/get-nth-square-number.spec.js
+++ js/square-numbers/get-nth-square-number.spec.js
@@ -3,5 +3,5 @@ const main = require('../..')
 const spec = require('../specs/get-nth-number-spec')
 
 describe('getNthSquareNumber', () => {
-  spec(main.getNthSquareNumber, 'S', [Big(1), Big(4), Big(9), Big(16), Big(25), Big(5329)])
+  spec(main.getNthSquareNumber, 'S', [Big('1'), Big('4'), Big('9'), Big('16'), Big('25'), Big('5329')])
 })
--- js/triangle-numbers/get-nth-triangle-number.spec.js
+++ js/triangle-numbers/get-nth-triangle-number.spec.js
@@ -3,5 +3,5 @@ const main = require('../..')
 const spec = require('../specs/get-nth-number-spec')
 
 describe('getNthTriangleNumber', () => {
-  spec(main.getNthTriangleNumber, 'T', [Big(1), Big(3), Big(6), Big(10), Big(15), Big(2701)])
+  spec(main.getNthTriangleNumber, 'T', [Big('1'), Big('3'), Big('6'), Big('10'), Big('15'), Big('2701')])
 })
