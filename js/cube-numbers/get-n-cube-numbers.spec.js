c6d82a07087c26bcf1fc2511c804b59666726839
Daniel Lewis <daniellewis777@gmail.com>
2018-12-22 01:34:35 +0000

    shared tests use big string

--- js/cube-numbers/get-n-cube-numbers.spec.js
+++ js/cube-numbers/get-n-cube-numbers.spec.js
@@ -5,5 +5,5 @@ const spec = require('../specs/get-n-numbers-spec')
 
 describe('getNCubeNumbers', function () {
   this.enableTimeouts(false)
-  spec(main.getNCubeNumbers, config.CUBE_NUMBERS_UPTO_100, Big(1000000000000000000))
+  spec(main.getNCubeNumbers, config.CUBE_NUMBERS_UPTO_100, Big('1000000000000000000'))
 })
