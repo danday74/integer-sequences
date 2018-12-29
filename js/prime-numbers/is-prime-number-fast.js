9483b634ce59e613a63aa718432f0820d5b542fd
Daniel Lewis <daniellewis777@gmail.com>
2018-12-29 00:11:17 +0000

    tidied code

--- js/prime-numbers/is-prime-number-fast.js
+++ js/prime-numbers/is-prime-number-fast.js
@@ -4,7 +4,7 @@ const isPrime = require('is-prime')
 const isPrimeNumberFast = (n = 0) => {
   n = Big(n)
   const num = parseInt(n.toFixed())
-  return n < 1 ? undefined : isPrime(num)
+  return n.lt(1) ? undefined : isPrime(num)
 }
 
 module.exports = isPrimeNumberFast
